e396d7689548c362c3a33d5cdead03e8971f08dc
Nitin Das <37865475+nitinda@users.noreply.github.com>
2021-04-01 16:45:40 +0100

    Modidifed code

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -4,11 +4,17 @@ import './Burger.css'
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients'
 
 const burger = (props) => {
+    const transfommedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />
+        })
+    });
+    console.log({transfommedIngredients})
+
     return (
         <div className="Burger">
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transfommedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     )
