a3487756e82832f86f65fda54bdae037689044a5
Nitin Das <37865475+nitinda@users.noreply.github.com>
2021-04-06 23:57:27 +0100

    Modified code

--- src/components/Burger/BuildControls/BuildControl/BuildControl.jsx
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
 import RemoveIcon from "@material-ui/icons/Remove";
+import Button from "@material-ui/core/Button";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,19 +18,40 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const StyledButton = withStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: 8,
+    border: 0,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    width: '120px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
+  label: {
+    textTransform: 'capitalize',
+  },
+})(Button);
+
 const BuildControl = (props) => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Fab color="secondary" aria-label="remove">
-        <RemoveIcon />
+      <Fab color="secondary" aria-label="remove" disabled={props.disabled}>
+        <RemoveIcon onClick={props.removed} />
       </Fab>
-      <Fab variant="extended" color="primary">
-          {props.label}
-      </Fab>
+      {/* <Button
+        variant="contained"
+        color="primary"
+        disableElevation
+      >
+        {props.label}{" "}
+      </Button> */}
+      <StyledButton>{props.label}</StyledButton>
       <Fab color="primary" aria-label="add">
-        <AddIcon />
+        <AddIcon onClick={props.added} />
       </Fab>
     </div>
   );
--- src/components/Burger/BuildControls/BuildControls.jsx
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 import BuildControl from "./BuildControl/BuildControl";
-// import "./BuildControls.css";
-
 import Typography from "@material-ui/core/Typography";
-// import Container from "@material-ui/core/Container";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+import "./BuildControls.css";
 
 const controls = [
   { label: "Salad", type: "salad" },
@@ -13,19 +16,60 @@ const controls = [
   { label: "Cheese", type: "cheese" },
 ];
 
+const StyledButton = withStyles({
+  root: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    borderRadius: 8,
+    border: 0,
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+    width: "190px",
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    "&$disabled": {
+      background: "linear-gradient(45deg, #607d8b 30%, #b0bec5 90%)",
+      borderRadius: 8,
+      border: 0,
+      color: "white",
+      height: 48,
+      padding: "0 30px",
+      width: "190px",
+      boxShadow: "0 3px 5px 2px rgba(33, 203, 243, .3)",
+    },
+  },
+  disabled: {},
+})(Button);
+
 const buildControls = (props) => {
   return (
-      <Typography
-        component="div"
-        style={{ height: "auto" }}
-        align="center"
-      >
-        <div className="BuildControls">
-          {controls.map((ctrl) => {
-            return <BuildControl key={ctrl.label} label={ctrl.label} />;
-          })}
-        </div>
-      </Typography>
+    <Typography component="div" style={{ height: "auto" }} align="center">
+      <div className="BuildControls">
+        <TextField
+          label="Current Price"
+          id="filled-start-adornment"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                $ {props.price.toFixed(2)}
+              </InputAdornment>
+            ),
+          }}
+          variant="outlined"
+        />
+        {controls.map((ctrl) => {
+          return (
+            <BuildControl
+              key={ctrl.label}
+              label={ctrl.label}
+              added={() => props.ingredientAdded(ctrl.type)}
+              removed={() => props.ingredientRemoved(ctrl.type)}
+              disabled={props.disabled[ctrl.type]}
+            />
+          );
+        })}
+        <StyledButton disabled={!props.purchasable}>ORDER NOW</StyledButton>
+      </div>
+    </Typography>
   );
 };
 
